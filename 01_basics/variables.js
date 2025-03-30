edc0860c36bb4729fe796149f32d75b1c0b96d3a
Anubhav025 <167561783+Anubhav025@users.noreply.github.com>
2025-03-30 06:40:09 +0000

    use case of variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = "155343"
+let accountEmail = "Anubhavraj@gmail.com"
+var accountPassword = "12345"
+accountCity = "Muzaffarpur"  //this is also possible to initiliazie in this way but it is not a good approch
+
+// accountId = "232436"
+
+// console.log(accountId)  // this is not possible to change because it is in const 
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+accountEmail = "honeySingh"
+accountPassword = "112233"
+accountCity = "bhagwanpur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
