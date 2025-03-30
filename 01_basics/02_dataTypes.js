dee846a33ae97cd3325e984094a962b087fe9651
Anubhav025 <167561783+Anubhav025@users.noreply.github.com>
2025-03-30 07:11:32 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+// types of primitive data types
+// int => range 2 to the pow 53
+// bigint => greater than the range of int
+// string => ""
+// boolen => true/false
+// symbol => To define the uniqness in the code
+// undefined => means the variable whoes value is not declared\
+// null => Means it is empty
+
+let name = "AnubhavRaj" //string
+let age = 21;  //int
+islogedIn = true;  // boolen
+let city;  // undefined
+let temp = null // empty
+
+console.table([name,age,islogedIn,city,temp])
+
+console.log(typeof temp)  
+
+//NOTE => the type of null dataType is object(imp)
+
