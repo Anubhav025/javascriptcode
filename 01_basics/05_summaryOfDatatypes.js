2f494056e7b4edc112a357e18574223e4cd338fa
Anubhav025 <167561783+Anubhav025@users.noreply.github.com>
2025-03-31 07:05:20 +0000

    summary of datatypes

---
+++ 01_basics/05_summaryOfDatatypes.js
@@ -0,0 +1,45 @@
+// There are two types of data types
+/*
+1. Primitive dataType
+a. Number
+b. String
+c. null
+d. undefined
+e. symbol
+f. BigInt
+g. boolen
+
+2. Refrence data type(Non primitive data type)
+
+a. Object
+b. Function
+c. Array
+
+*/
+
+// how to initilize it
+
+const score = 100
+const Maxscore = 100.3
+
+const name = "Anubhav Raj"
+
+const Temp = null
+
+let city;
+
+const Id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(Id==anotherId);
+
+
+const heros = ["Anubhav","hitesh","supriya"]; //Array
+
+let myObj = {  //object
+    name :"Anubhav",
+    age : 21,
+    roll: 123
+}
+ const myfunction = function(){
+    console.log("Hello World");
+ }
