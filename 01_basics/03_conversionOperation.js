8ef0db5a4b7bae175adcf273d414ca0a4053e3cd
Anubhav025 <167561783+Anubhav025@users.noreply.github.com>
2025-03-31 05:58:39 +0000

    conversion And Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,57 @@
+//********************** CONVERSION ************************************ */
+
+let score = null
+let somenumber = Number(score)
+// console.log(typeof somenumber);
+// console.log(somenumber);
+
+// string to number
+// "33abc" => type is number but it not give a number while printing it gives NaN(not a number)
+
+
+// bollen to number
+// true => number and ans 1
+
+// null to number
+// null => number and ans is 0
+
+let isloggedIn = "AnubhavRaj"
+let bollenisLoggedIn = Boolean(isloggedIn);
+// console.log(typeof bollenisLoggedIn);
+// console.log(bollenisLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+//"AnubhavRaj" => true
+
+//***************************** OPERATION **************************//
+
+str1 = "Hello"
+str2 = " Anubhav Raj"
+str3 = str1 + str2
+// console.log(str3);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // double ** used for power
+// console.log(2/2);
+// console.log(2%2);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"4"); //34
+
+//NOTE => jo v kaam hoga wo phaala operation ko dhak ka hee hoga 
+
+let decision = true
+// console.log(decision); // output = true
+
+let decision1 = +true // here + is conversion operator
+// console.log(decision1);  // output = 1
+
+let num = 56;
+num++
+console.log(num);
+
