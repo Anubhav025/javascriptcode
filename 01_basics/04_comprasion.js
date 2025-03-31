625ec8d68d017997d9506066bdb6eacb2c3c52a4
Anubhav025 <167561783+Anubhav025@users.noreply.github.com>
2025-03-31 06:17:00 +0000

    comprasion

---
+++ 01_basics/04_comprasion.js
@@ -0,0 +1,12 @@
+// NOTE => comparing same data type is not a problem but comparing different data types 
+//   gives the problem of understanding
+
+// console.log("2" == 2); //it is true because in double == means it just compare the value
+// console.log("02" > 2);
+
+// console.log("2"===2); //it is false because intripple(===) it not just compare the value it also compare the data type  
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
