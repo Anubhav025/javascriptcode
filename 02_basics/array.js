da23fd0424220bd4c95e4b487e1819f6fe61eb81
Anubhav025 <167561783+Anubhav025@users.noreply.github.com>
2025-04-09 13:27:41 +0000

    Array

---
+++ 02_basics/array.js
@@ -0,0 +1,12 @@
+const Myarr = [1,2,3,4,5]
+console.log(Myarr)
+
+const newarr = new Array(1,2,3,4)
+console.log(newarr[0]);
+
+//Array Methods 
+
+Myarr.push(6) // push the element to the last off the array
+
+Myarr.pop()
+console.log(Myarr);
